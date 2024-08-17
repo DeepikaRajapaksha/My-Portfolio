023f525f13297a02e95c105e9af0fed909d429c1
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2024-08-17 19:08:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-    return (
-        <Router>
-            <Header />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/projects" component={Projects} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
