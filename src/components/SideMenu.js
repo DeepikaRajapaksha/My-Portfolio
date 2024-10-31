31e13b70b4eb783ea9b56907313fabcdb5fb710b
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2024-11-01 00:25:30 +0530

    Remove close button

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -7,9 +7,7 @@ const SideMenu = ({ isOpen, closeMenu }) => {
         isOpen ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
-      <button onClick={closeMenu} className="absolute top-6 right-6">
-        <img src="/assets/img/close.png" alt="Close" />
-      </button>
+      
       <ul className="flex flex-col items-center justify-center h-full space-y-4">
         <li><a href="#top" onClick={closeMenu}>Home</a></li>
         <li><a href="#aboutme" onClick={closeMenu}>About Me</a></li>
