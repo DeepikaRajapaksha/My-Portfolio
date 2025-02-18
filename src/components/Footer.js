5e6350a3847b9688dfbbeca66780e513cf5c6b08
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-02-18 09:12:32 +0530

    Set Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="py-4 text-center">
-      <p>© 2024 Deepika Sewwandi. All rights reserved.</p>
       <div className="flex justify-center gap-4 mt-2">
-        <a href="https://github.com/deepika-sewwandi">GitHub</a>
-        <a href="https://linkedin.com/in/deepika-sewwandi">LinkedIn</a>
-        <a href="https://instagram.com/deepika_sewwandi">Instagram</a>
+        <a href="https://www.behance.net/deepika_sewwandi">GitHub</a>
+        <a href="https://www.linkedin.com/in/deepika-sewwandi-799aa6343/">LinkedIn</a>
       </div>
+      <p>© 2024 Deepika Sewwandi. All rights reserved.</p>
     </footer>
   );
 };
